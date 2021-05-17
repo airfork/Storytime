383b7f4c8d2828efe7feb1b6689536176d6b578d
Tunji Afolabi-Brown <airfork@gmail.com>
2021-05-17 07:12:55 -0400

    initial commit, saving progress

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+// fn main() -> std::io::Result<()> {
+//     let mut reader = my_reader::BufReader::open("test.txt")?;
+//     let mut buffer = String::new();
+
+//     while let Some(line) = reader.read_line(&mut buffer) {
+//         println!("{}", line?.trim());
+//     }
+
+//     Ok(())
+// }
+
+use std::io::{stdout, Write};
+
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand, Result,
+    event,
+};
+
+fn main() -> Result<()> {
+    // or using functions
+    stdout()
+        .execute(SetForegroundColor(Color::Blue))?
+        .execute(SetBackgroundColor(Color::Red))?
+        .execute(Print("Styled text here."))?
+        .execute(ResetColor)?;
+
+    Ok(())
+}
+
+mod my_reader {
+    use std::{
+        fs::File,
+        io::{self, prelude::*},
+    };
+
+    pub struct BufReader {
+        reader: io::BufReader<File>,
+    }
+
+    impl BufReader {
+        pub fn open(path: impl AsRef<std::path::Path>) -> io::Result<Self> {
+            let file = File::open(path)?;
+            let reader = io::BufReader::new(file);
+
+            Ok(Self { reader })
+        }
+
+        pub fn read_line<'buf>(
+            &mut self,
+            buffer: &'buf mut String,
+        ) -> Option<io::Result<&'buf mut String>> {
+            buffer.clear();
+
+            self.reader
+                .read_line(buffer)
+                .map(|u| if u == 0 { None } else { Some(buffer) })
+                .transpose()
+        }
+    }
+}
